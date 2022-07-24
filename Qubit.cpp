a389b43b8594236814fe197fbb94d3c1a003007a
haimasker <xxxmelnikxxx@gmail.com>
2022-07-24 12:47:16 +0300

    Completed

---
+++ Qubit.cpp
@@ -0,0 +1,332 @@
+#include "Qubit.h"
+
+Qubit::Qubit(unsigned qubits) noexcept : qubitNumber(qubits) {
+	generator.seed((unsigned)time(NULL));
+	if (qubits != 0)
+		amplitudes = Matrix(1, (unsigned)pow(2, qubits), sqrt(1.0 / pow(2, qubits)));
+}
+
+Qubit::Qubit(const Qubit& q) noexcept {
+	this->qubitNumber = q.qubitNumber;
+	this->amplitudes = q.amplitudes;
+	this->generator = q.generator;
+}
+
+Qubit::~Qubit() {}
+
+std::complex<double>& Qubit::operator () (const unsigned& index) {
+	return this->amplitudes(0, index);
+}
+
+const std::complex<double>& Qubit::operator () (const unsigned& index) const {
+	return this->amplitudes(0, index);
+}
+
+Qubit& Qubit::operator = (const Qubit& q) {
+	if (this == &q || this->amplitudes == q.amplitudes)
+		return *this;
+	this->amplitudes = q.amplitudes;
+	this->qubitNumber = q.qubitNumber;
+	return *this;
+}
+
+Qubit Qubit::operator * (const Qubit& q) {
+	if (pow(2, this->qubitNumber) * pow(2, q.qubitNumber) > MAX_QUBITS) {
+		std::cout << "Qubits limit exceeded\n";
+		return *this;
+	}
+	Qubit res(this->qubitNumber + q.qubitNumber);
+	for (unsigned i = 0; i < pow(2, this->qubitNumber); i++)
+		for (unsigned j = 0; j < pow(2, q.qubitNumber); j++)
+			res.amplitudes(0, i * q.qubitNumber + j) = this->amplitudes(0, i) * q.amplitudes(0, j);
+	return res;
+}
+
+Qubit& Qubit::operator *= (const Qubit& q) {
+	Qubit res = (*this) * q;
+	(*this) = res;
+	return *this;
+}
+
+bool Qubit::operator == (const Qubit& q) {
+	return this->amplitudes == q.amplitudes;
+}
+
+bool Qubit::operator != (const Qubit& q) {
+	return this->amplitudes != q.amplitudes;
+}
+
+Qubit Qubit::operator * (Matrix& m) {
+	if (this->amplitudes.getCols() != m.getRows())
+		return *this;
+	Qubit res(this->qubitNumber);
+	Matrix ampl(this->amplitudes.getRows(), m.getCols(), 0.0 + 0.0i);
+	for (unsigned i = 0; i < this->amplitudes.getRows(); i++)
+		for (unsigned j = 0; j < m.getCols(); j++)
+			for (unsigned k = 0; k < m.getRows(); k++)
+				ampl(i, j) += this->amplitudes(i, k) * m(k, j);
+	res.amplitudes = ampl;
+	return res;
+}
+
+Qubit& Qubit::operator *= (Matrix& m) {
+	Qubit res = (*this) * m;
+	(*this) = res;
+	return *this;
+}
+
+bool Qubit::operator == (const Matrix& m) {
+	return this->amplitudes == m;
+}
+
+bool Qubit::operator != (const Matrix& m) {
+	return this->amplitudes != m;
+}
+
+void Qubit::concentrate(unsigned index) {
+	for (unsigned i = 0; i < this->amplitudes.getMat()[0].size(); i++)
+		this->amplitudes.getMat()[0][i % this->amplitudes.getMat()[0].size()] = 0 + 1 * (i == index);
+}
+
+void Qubit::reset() {
+	this->amplitudes = Matrix(1, (unsigned)pow(2, this->qubitNumber), sqrt(1.0 / pow(2, this->qubitNumber)));
+	return;
+}
+
+Matrix Qubit::getAmplitudes() {
+	return this->amplitudes;
+}
+
+unsigned Qubit::getQubitNumber() {
+	return this->qubitNumber;
+}
+
+std::vector<double> Qubit::getProbabilities() {
+	std::vector<double> res;
+	std::vector<std::complex<double>> ampl = (this->amplitudes).getMat()[0];
+	for (std::complex<double> i : ampl)
+		res.push_back(norm(i));
+	return res;
+}
+
+Matrix Qubit::identity() {
+	return Matrix::identity(2);
+}
+
+bool Qubit::identity(Qubit& q) {
+	Matrix I = Matrix::identity((unsigned)pow(2, q.qubitNumber));
+	q *= I;
+	return true;
+}
+
+Matrix Qubit::pauliX() {
+	Matrix X(2, 2);
+	X(0, 1) = X(1, 0) = 1;
+	return X;
+}
+
+bool Qubit::pauliX(Qubit& q) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix X = pauliX();
+	q *= X;
+	return true;
+}
+
+Matrix Qubit::pauliY() {
+	Matrix Y(2, 2);
+	Y(0, 1) = std::complex<double>(0, -1);
+	Y(1, 0) = std::complex<double>(0, 1);
+	return Y;
+}
+
+bool Qubit::pauliY(Qubit& q) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix Y = pauliY();
+	q *= Y;
+	return true;
+}
+
+Matrix Qubit::pauliZ() {
+	Matrix Z(2, 2);
+	Z(0, 0) = 1;
+	Z(1, 1) = -1;
+	return Z;
+}
+
+bool Qubit::pauliZ(Qubit& q) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix Z = pauliZ();
+	q *= Z;
+	return true;
+}
+
+Matrix Qubit::hadamard() {
+	Matrix H(2, 2, 1);
+	H(1, 1) = -1;
+	H *= std::complex<double>(1 / sqrt(2), 0);
+	return H;
+}
+
+bool Qubit::hadamard(Qubit& q) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix H = hadamard();
+	q *= H;
+	return true;
+}
+
+Matrix Qubit::phaseShift(double angle) {
+	Matrix P(2, 2);
+	P(0, 0) = 1;
+	P(1, 1) = exp(std::complex<double>(0, 1) * angle);
+	return P;
+}
+
+bool Qubit::phaseShift(Qubit& q, double angle) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix P = phaseShift(angle);
+	q *= P;
+	return true;
+}
+
+Matrix Qubit::swap() {
+	Matrix S(4, 4);
+	S(0, 0) = S(1, 2) = S(2, 1) = S(3, 3) = 1;
+	return S;
+}
+
+bool Qubit::swap(Qubit& q) {
+	if (q.qubitNumber != 2)
+		return false;
+	Matrix S = swap();
+	q *= S;
+	return true;
+}
+
+Matrix Qubit::rotX(double angle) {
+	Matrix R(2, 2);
+	R(0, 0) = R(1, 1) = cos(angle / 2);
+	R(0, 1) = R(1, 0) = std::complex<double>(0, -1) * sin(angle / 2);
+	return R;
+}
+
+bool Qubit::rotX(Qubit& q, double angle) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix R = rotX(angle);
+	q *= R;
+	return true;
+}
+
+Matrix Qubit::rotY(double angle) {
+	Matrix R(2, 2);
+	R(0, 0) = R(1, 1) = cos(angle / 2);
+	R(0, 1) = -sin(angle / 2);
+	R(1, 0) = sin(angle / 2);
+	return R;
+}
+
+bool Qubit::rotY(Qubit& q, double angle) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix R = rotY(angle);
+	q *= R;
+	return true;
+}
+
+Matrix Qubit::rotZ(double angle) {
+	Matrix R(2, 2);
+	R(0, 0) = exp(std::complex<double>(0, -1) * (angle / 2));
+	R(1, 1) = exp(std::complex<double>(0, 1) * (angle / 2));
+	return R;
+}
+
+bool Qubit::rotZ(Qubit& q, double angle) {
+	if (q.qubitNumber != 1)
+		return false;
+	Matrix R = rotZ(angle);
+	q *= R;
+	return true;
+}
+
+Matrix Qubit::controlled(unsigned qubits, std::string gate, std::string angle) {
+	if (gate == "identity")
+		return Matrix((unsigned)pow(2, qubits), (unsigned)pow(2, qubits), 1);
+	Matrix C((unsigned)pow(2, qubits), (unsigned)pow(2, qubits));
+	for (unsigned i = 0; i < (unsigned)pow(2, qubits) - 2; i++)
+		C(i, i) = 1;
+	Matrix lastQubit;
+	if (gate == "pauliX")
+		lastQubit = Qubit::pauliX();
+	else if (gate == "pauliY")
+		lastQubit = Qubit::pauliY();
+	else if (gate == "pauliZ")
+		lastQubit = Qubit::pauliZ();
+	else if (gate == "hadamard")
+		lastQubit = Qubit::hadamard();
+	else if (gate == "swap")
+		lastQubit = Qubit::swap();
+	else if (gate == "phaseShift")
+		lastQubit = Qubit::phaseShift(std::stod(angle));
+	else if (gate == "rotX")
+		lastQubit = Qubit::rotX(std::stod(angle));
+	else if (gate == "rotY")
+		lastQubit = Qubit::rotY(std::stod(angle));
+	else if (gate == "rotZ")
+		lastQubit = Qubit::rotZ(std::stod(angle));
+	else
+		return Matrix::identity((unsigned)pow(2, qubits));
+
+	for (unsigned i = (unsigned)pow(2, qubits) - 1; i > (unsigned)pow(2, qubits) - 3 - 2 * (gate == "swap"); i--)
+		for (unsigned j = (unsigned)pow(2, qubits) - 1; j > (unsigned)pow(2, qubits) - 3 - 2 * (gate == "swap"); j--)
+			C(i, j) = lastQubit(i % lastQubit.getRows(), j % lastQubit.getCols());
+	return C;
+}
+
+bool Qubit::controlled(Qubit& q, unsigned qubits, std::string gate, std::string angle) {
+	if (q.qubitNumber != qubits)
+		return false;
+	Matrix C = controlled(qubits, gate, angle);
+	q *= C;
+	return true;
+}
+
+void Qubit::measure() {
+	std::vector<double> prob = this->getProbabilities();
+	std::discrete_distribution<int> distribution(prob.begin(), prob.end());
+	std::cout << "|" << std::bitset<MAX_QUBITS>(distribution(this->generator))
+		.to_string()
+		.substr(MAX_QUBITS - this->qubitNumber) << ">\n";
+	return;
+}
+
+void Qubit::printProbabilities() {
+	std::vector<double> prob = this->getProbabilities();
+	for (double i : prob)
+		std::cout << i << ' ';
+	std::cout << "\n";
+}
+
+std::ostream& operator << (std::ostream& out, Qubit& q) {
+	if (q.qubitNumber != 0) {
+		for (size_t i = 0; i < q.amplitudes.getMat()[0].size(); i++) {
+			if (q.amplitudes.getMat()[0][i].imag() == 0 && q.amplitudes.getMat()[0][i].real() == 0)
+				continue;
+			if (i)
+				printf(" + ");
+			out << q.amplitudes.getMat()[0][i];
+			out << "|" << std::bitset<MAX_QUBITS>(i)
+				.to_string()
+				.substr(MAX_QUBITS - q.qubitNumber) << ">";
+		}
+		out << "\n";
+	}
+	else
+		out << "Empty qubit register\n";
+	out << "\n";
+	return out;
+}
---
+++ Qubit.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <iostream>
+#include <bitset>
+#include <complex>
+#include <numeric>
+#include <random>
+#include <ctime>
+#include "matrix.h"
+
+using namespace std::complex_literals;
+
+#define MAX_QUBITS 8
+
+class Qubit {
+
+private:
+	unsigned	qubitNumber;
+	Matrix		amplitudes;
+	std::default_random_engine generator;
+
+public:
+	Qubit(unsigned = 0) noexcept;
+	Qubit(const Qubit&) noexcept;
+
+	virtual ~Qubit();
+
+	std::complex<double>&		operator () (const unsigned&);
+	const std::complex<double>&	operator () (const unsigned&) const;
+
+	Qubit&	operator = (const Qubit&);
+	Qubit	operator * (const Qubit&);
+	Qubit&	operator *= (const Qubit&);
+
+	bool operator == (const Qubit&);
+	bool operator != (const Qubit&);
+
+	Qubit	operator * (Matrix&);
+	Qubit&	operator *= (Matrix&);
+
+	bool operator == (const Matrix&);
+	bool operator != (const Matrix&);
+
+	void concentrate(unsigned);
+	void reset();
+
+	Matrix				getAmplitudes();
+	unsigned			getQubitNumber();
+	std::vector<double>	getProbabilities();
+
+	static Matrix	identity();
+	static bool		identity(Qubit&);
+
+	static Matrix	pauliX();
+	static bool		pauliX(Qubit&);
+
+	static Matrix	pauliY();
+	static bool		pauliY(Qubit&);
+
+	static Matrix	pauliZ();
+	static bool		pauliZ(Qubit&);
+
+	static Matrix	hadamard();
+	static bool		hadamard(Qubit&);
+
+	static Matrix	phaseShift(double);
+	static bool		phaseShift(Qubit&, double);
+
+	static Matrix	swap();
+	static bool		swap(Qubit&);
+
+	static Matrix	rotX(double);
+	static bool		rotX(Qubit&, double);
+
+	static Matrix	rotY(double);
+	static bool		rotY(Qubit&, double);
+
+	static Matrix	rotZ(double);
+	static bool		rotZ(Qubit&, double);
+
+	static Matrix	controlled(unsigned, std::string, std::string = "");
+	static bool		controlled(Qubit&, unsigned, std::string, std::string = "");
+
+	void measure();
+
+	void printProbabilities();
+
+	friend std::ostream& operator << (std::ostream&, Qubit&);
+};
